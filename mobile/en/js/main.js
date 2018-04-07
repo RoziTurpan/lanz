2c8a991a760dcdde650f5b7d803b716e127dd68f
RoziTurpan <291287374@qq.com>
2018-04-08 00:50:54 +0800

    some optimizations

--- mobile/en/js/main.js
+++ mobile/en/js/main.js
@@ -23,35 +23,42 @@ $(function(){
 	})
 
 	/*判断滚动条滑动方向*/
-	var direct = 0; //默认向下
-	var scrollDirection = function (e) {
-		e = e || window.event;
-		if (e.wheelDelta) {  //判断浏览器IE，谷歌滑轮事件               
-			if (e.wheelDelta > 0) { //当滑轮向上滚动时  
-				direct = 1;
-			}
-			if (e.wheelDelta < 0) { //当滑轮向下滚动时  
-				direct = 0;
-			}
-		} else if (e.detail) {  //Firefox滑轮事件  
-			if (e.detail > 0) { //当滑轮向上滚动时  
-				direct = 0;
-			}
-			if (e.detail < 0) { //当滑轮向下滚动时  
-				direct = 1;
-			}
+	var direct = 1; // 默认往下
+	var startX, startY, X, Y;
+	$("body").on("touchstart", function (e) {
+		e.preventDefault();
+		startX = e.originalEvent.changedTouches[0].pageX,
+			startY = e.originalEvent.changedTouches[0].pageY;
+	});
+	$("body").on("touchmove", function (e) {
+		e.preventDefault();
+		moveEndX = e.originalEvent.changedTouches[0].pageX,
+			moveEndY = e.originalEvent.changedTouches[0].pageY,
+			X = moveEndX - startX,
+			Y = moveEndY - startY;
+
+		if (Math.abs(X) > Math.abs(Y) && X > 0) {
+			// alert("left 2 right");
 		}
-	}
-	//给页面绑定滑轮滚动事件  
-	if (document.addEventListener) {//firefox  
-		document.addEventListener('DOMMouseScroll', scrollDirection, false);
-	}
-	//滚动滑轮触发scrollDirection方法  //ie 谷歌  
-	window.onmousewheel = document.onmousewheel = scrollDirection;
+		else if (Math.abs(X) > Math.abs(Y) && X < 0) {
+			// alert("right 2 left");
+		}
+		else if (Math.abs(Y) > Math.abs(X) && Y > 0) {
+			// alert("top 2 bottom");
+			direct = 1;
+		}
+		else if (Math.abs(Y) > Math.abs(X) && Y < 0) {
+			// alert("bottom 2 top");
+			direct = 0;
+		}
+		else {
+			// alert("just touch");
+		}
+	});
 
 	var navObj = $('header');
 	var lH = navObj.offset().top;
-	$(window).on('scroll', function () {
+	window.addEventListener('scroll', function () {
 		if (direct == 1) {
 			if ($(window).scrollTop() > lH) {
 				navObj.addClass('fixed-nav');
@@ -61,6 +68,5 @@ $(function(){
 		} else {
 			navObj.removeClass('fixed-nav');
 		}
-
-	});
+	})
 })
